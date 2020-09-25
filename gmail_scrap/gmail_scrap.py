f40ae58c61abaa1365956a4b606bef260c4ceae9
TSS <tss@TSSs-MacBook-Air.local>
2020-09-25 17:16:55 +0530

    keywords search

--- gmail_scrap/gmail_scrap.py
+++ gmail_scrap/gmail_scrap.py
@@ -5,8 +5,10 @@ import sys
 import json
 
 class GmailFinin():
+
     def helloWorld(self):
         print("\nHello I'm here to help you")
+
 
     def initializeVariables(self):
         self.usr = ""
@@ -18,11 +20,14 @@ class GmailFinin():
         self.data = []
         self.ids = []
         self.idsList = []
+        self.keywords = ['invoice', 'bill', 'subscription alert', 'payment', 'money', 'loan']
+
 
     def getLogin(self):
         print("\nPlease enter your Gmail login details below.")
         self.usr = input("Email: ")
         self.pwd = getpass.getpass("Enter your password --> ")
+
 
     def attemptLogin(self):
         self.mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
@@ -35,74 +40,87 @@ class GmailFinin():
             print("\nLogon FAILED")
             return False
 
+
     def checkIfUsersWantsToContinue(self):
-    	# return True
-    	print("\nWe have found "+str(self.mailCount)+" emails in the mailbox "+self.mailbox+".")
-    	return True if input("Do you wish to continue extracting all the emails into "+self.destFolder+"? (y/N) ").lower().strip()[:1] == "y" else False       
-        
+        print("\nWe have found "+str(len(self.idsList))+" emails in the mailbox "+self.mailbox+".")
+        return True if input("Do you wish to continue extracting all the emails into "+self.destFolder+"? (y/N) ").lower().strip()[:1] == "y" else False
+
+
     def selectMailbox(self):
         self.mailbox = input("\nPlease type the name of the mailbox you want to extract, e.g. Inbox: ")
-        # self.mailbox = "Inbox"
         bin_count = self.mail.select(self.mailbox)[1]
         self.mailCount = int(bin_count[0].decode("utf-8"))
         return True if self.mailCount > 0 else False
 
+
     def searchThroughMailbox(self):
-        type, self.data = self.mail.search(None, "ALL")
-        self.ids = self.data[0]
-        self.idsList = self.ids.split()
+        if not len(self.keywords):
+            type, self.data = self.mail.search(None, "ALL")
+        else:
+            for keys in self.keywords:
+                type, self.data = self.mail.search(None, '(OR TEXT "'+keys+'" SUBJECT "'+keys+'")')
+                # type, self.data = self.mail.search(None, '(OR TEXT "invoice" TEXT "bills")')
+                self.ids = self.data[0]
+                self.idsList += self.ids.split()
+
 
     def parseEmails(self):
         jsonOutput = {}
-        for anEmail in self.data[0].split()[::-1]:
-        	type, self.data = self.mail.fetch(anEmail, '(UID RFC822)')
-        	raw = self.data[0][1]
-        	try:
-        		raw_str = raw.decode("utf-8")
-        	except UnicodeDecodeError:
-        		try:
-        			raw_str = raw.decode("ISO-8859-1")
-        		except UnicodeDecodeError:
-        			try:
-        				raw_str = raw.decode("ascii")
-        			except UnicodeDecodeError:
-        				pass
-        	
-        	msg = email.message_from_string(raw_str)
+        # for anEmail in self.data[0].split()[::-1]:
+        for anEmail in self.idsList:
+            pass
+            type, self.data = self.mail.fetch(anEmail, '(UID RFC822)')
+            raw = self.data[0][1]
+            try:
+                raw_str = raw.decode("utf-8")
+            except UnicodeDecodeError:
+                try:
+                    raw_str = raw.decode("ISO-8859-1")
+                except UnicodeDecodeError:
+                    try:
+                        raw_str = raw.decode("ascii")
+                    except UnicodeDecodeError:
+                        pass
 
-        	jsonOutput['subject'] = msg['subject']
-        	jsonOutput['from'] = msg['from']
-        	jsonOutput['date'] = msg['date']
 
-        	raw = self.data[0][0]
-        	raw_str = raw.decode("utf-8")
+            msg = email.message_from_string(raw_str)
 
-        	uid = raw_str.split()[2]
-        	# Body 
-        	if msg.is_multipart():
-        		for part in msg.walk():
-        			partType = part.get_content_type()
-        			## Get Body ##
-        			if partType == "text/plain" and "attachment" not in part:
-        				jsonOutput['body'] = part.get_payload()
-        			## Get Attachments ##
-        			if part.get('Content-Disposition') is not None:
-        				attchName = part.get_filename()
-        				if bool(attchName):
-        					attchFilePath = str(self.destFolder)+str(uid)+str("/")+str(attchName)
-        					os.makedirs(os.path.dirname(attchFilePath), exist_ok=True)
-        					with open(attchFilePath, "wb") as f:
-        						f.write(part.get_payload(decode=True))
+            jsonOutput['subject'] = msg['subject']
+            jsonOutput['from'] = msg['from']
+            jsonOutput['date'] = msg['date']
 
-        	else:
-        		jsonOutput['body'] = msg.get_payload()
+            raw = self.data[0][0]
+            raw_str = raw.decode("utf-8")
 
-        	outputDump = json.dumps(jsonOutput)
-        	emailInfoFilePath = str(self.destFolder)+str(uid)+str("/")+str(uid)+str(".json")
-        	os.makedirs(os.path.dirname(emailInfoFilePath), exist_ok=True)
-        	print(uid)
-        	with open(emailInfoFilePath, "w") as f:
-        		f.write(outputDump)
+            uid = raw_str.split()[2]
+
+            # Body 
+            if msg.is_multipart():
+                for part in msg.walk():
+                    partType = part.get_content_type()
+                    ## Get Body ##
+                    if partType == "text/plain" and "attachment" not in part:
+                        jsonOutput['body'] = part.get_payload()
+
+                    ## Get Attachments ##
+                    if part.get('Content-Disposition') is not None:
+                        attchName = part.get_filename()
+                        if bool(attchName):
+                            attchFilePath = str(self.destFolder)+str(uid)+str("/")+str(attchName)
+                            os.makedirs(os.path.dirname(attchFilePath), exist_ok=True)
+                            with open(attchFilePath, "wb") as f:
+                                f.write(part.get_payload(decode=True))
+
+            else:
+                jsonOutput['body'] = msg.get_payload()
+
+            outputDump = json.dumps(jsonOutput)
+            # print(jsonOutput)
+            emailInfoFilePath = str(self.destFolder)+str(uid)+str("/")+str(uid)+str(".json")
+            os.makedirs(os.path.dirname(emailInfoFilePath), exist_ok=True)
+            print(uid)
+            with open(emailInfoFilePath, "w") as f:
+                f.write(outputDump)
 
     def __init__(self):
         self.initializeVariables()
@@ -112,8 +130,8 @@ class GmailFinin():
             not self.selectMailbox() and sys.exit()
         else:
             sys.exit()
-        not self.checkIfUsersWantsToContinue() and sys.exit()
         self.searchThroughMailbox()
+        not self.checkIfUsersWantsToContinue() and sys.exit()
         self.parseEmails()
 
 if __name__ == "__main__":
